45d84b80fd5e1624a20e58f8fd631dd37cdf4434
rqcao <rqcao@outlook.com>
2018-04-21 14:51:45 +0800

    add /hello

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
+const logger = require('../utils/loggers/logger');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  logger.info(`url:${req.originalUrl} || ${req.ip}`);
+  res.render('index', {title: 'Express'});
 });
+
+router.get('/login', (req, res, next) => {
+  logger.info(`url:${req.originalUrl} || ${req.ip}`);
+  res.set('set-Cookie', `username=${req.query.username}`);
+  res.send();
+});
+router.get('/hello', (req, res, next) => {
+  if (req.cookies.username) {
+    res.send(`<h1>${req.cookies.username}</h1>`)
+  }
+});
+
+
 
 module.exports = router;
