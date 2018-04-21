11c708e28b1bb2e5297e7182fd985135a6c18c05
rqcao <rqcao@outlook.com>
2018-04-21 22:27:52 +0800

    add login page

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,64 @@ const router = express.Router();
 
 const logger = require('../utils/loggers/logger');
 
+const JWT = require('jsonwebtoken');
+
+const crypto = require('crypto');
+const User = require('../models/mongoose/user');
+const bluebird = require('bluebird');
+const pbkdf2Async = bluebird.promisify(crypto.pbkdf2);
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   logger.info(`url:${req.originalUrl} || ${req.ip}`);
-  res.render('index', {title: 'Express'});
+  res.render('index', {title: 'Ricky的博客'});
 });
 
 router.get('/login', (req, res, next) => {
   logger.info(`url:${req.originalUrl} || ${req.ip}`);
-  res.set('set-Cookie', `username=${req.query.username}`);
-  res.send();
-});
-router.get('/hello', (req, res, next) => {
-  if (req.cookies.username) {
-    res.send(`<h1>${req.cookies.username}</h1>`)
-  }
+  res.render('login', {title: 'Express'});
 });
 
+
+router.post('/login', (req, res, next) => {
+
+  (async () => {
+    const {name,age,password} = req.body;
+    const cipher = await pbkdf2Async(password,'ashdjkaqkjwjehasd',10000,512,'sha256');
+    const created = await User.insert({name,age,password:cipher});
+    return created;
+  })()
+      .then(r => {
+        res.json('创建成功');
+      })
+      .catch(e => {
+        next(e);
+      });
+});
+
+
+//jwt测试
+router.post('/jwt', (req, res, next) => {
+  // logger.info(`url:${req.originalUrl} || ${req.ip}`);
+  // res.set('set-Cookie', `username=${req.query.username}`);
+  // res.send();
+  //const user = {username,expiredAt:Date.now().valueOf() + 20*60*1000};
+  //const token = JWT.sign(user, 'qweqwwweqweqwe');
+  //res.send(token);
+
+});
+router.post('/hello', (req, res, next) => {
+  const auth = req.get('Authorization');
+  if(!auth) res.send('no auth!!');
+  if(auth.indexOf('Bearer ') === -1) res.send('no bearer auth!!');
+  const token = auth.split('Bearer ')[1];
+  const user = JWT.verify(token,'qweqwwweqweqwe');
+  if(user.expiredAt < Date.now().valueOf()) res.send('no bearer auth!!');
+  res.send(user);
+  // if (req.cookies.username) {
+  //   res.send(`<h1>${req.cookies.username}</h1>`)
+  // }
+});
 
 
 module.exports = router;
