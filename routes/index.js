bff33cde8bcc8b3b276d7e2ba57e70be16073b58
rqcao <rqcao@outlook.com>
2018-06-08 16:18:19 +0800

    🔨add uikit test page,✨decide to use uikit

--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,22 @@ const pbkdf2Async = bluebird.promisify(crypto.pbkdf2);
 /* GET home page. */
 router.get('/', (req, res, next) => {
   logger.info(`url:${req.originalUrl} || ${req.ip}`);
-  // if(!req.cookies.username){
-  //   res.redirect('/login');
-  //   return;
-  // }
-  res.render('index', {title: 'Ricky的博客'});
+  let users = [{
+    name:'q',
+    age:1
+  },{
+    name:'w',
+    age:2
+  },{
+    name:'e',
+    age:3
+  },{
+    name:'r',
+    age:4
+  }];
+  //res.locals 可以传变量过去，pm2 start .bin/www --watch  一定要加watch 去监控文件的变动
+  //res.locals.testu = users;
+  res.render('index', {title: 'Ricky的博客啊',users:users});
 });
 
 router.get('/login', (req, res, next) => {
@@ -41,7 +52,5 @@ router.get('/register', (req, res, next) => {
   logger.info(`url:${req.originalUrl} || ${req.ip}`);
   res.render('register', {title: '注册页面'});
 });
-
-
 
 module.exports = router;
