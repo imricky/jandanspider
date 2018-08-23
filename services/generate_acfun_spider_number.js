62a11e887599698a6ee30bc7b2e80a192d5e54d0
rqcao <rqcao@outlook.com>
2018-08-23 20:22:21 +0800

    ✨ add project

--- services/generate_acfun_spider_number.js
+++ services/generate_acfun_spider_number.js
@@ -2,19 +2,19 @@ const Redis = require('ioredis');
 const redis = new Redis();
 
 (async function generateIDsToRedis() {
-  for(let i = 0;i<450;i++){
+  for (let i = 0; i < 450; i++) {
     const arr = new Array(10000);
-    for(let j = 0;j<10000;j++){
-      arr.push(i*10000+j);
+    for (let j = 0; j < 10000; j++) {
+      arr.push(i * 10000 + j);
     }
-    await redis.sadd('acfun_ids_generate',...arr);
+    await redis.sadd('acfun_ids_generate', ...arr);
   }
 })()
-  .then(r => {
-    console.log('done')
-  })
-  .catch(e => {
-  });
+    .then(r => {
+      console.log('done');
+    })
+    .catch(e => {
+    });
 
 
 
