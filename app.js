f5ee97cfcd731fa00915274d72da9a9a4526d748
rqcao <rqcao@outlook.com>
2018-09-11 16:34:49 +0800

    ✨ 添加index.ejs 中的nav导航，准备开始写页面

--- app.js
+++ app.js
@@ -87,7 +87,6 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
   logger.error(err.stack)
-  // console.log('您访问的资源不存在!');
   // render the error page
   res.status(err.status || 500)
   res.render('error')
